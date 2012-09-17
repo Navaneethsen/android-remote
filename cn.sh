f1c8d0d47ccbe40be758fd17390ed73fdfde6f98
kureikain <kureikain@gmail.com>
2012-09-17 15:37:20 -0700

    swithch between 2 profiles

---
+++ cn.sh
@@ -0,0 +1,3 @@
+#!/bin/zsh
+git config user.name "Vinh"
+git config user.emaail "vinh.nguyen@aronics.com"
---
+++ gh.sh
@@ -0,0 +1,3 @@
+#!/bin/zsh
+git config user.name "kureikain"
+git config user.emaail "kureikain@gmail.com"
