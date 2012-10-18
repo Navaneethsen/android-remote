5b1e107a18c875239efe25d0fdb80ef06ee81696
Duc Phan <duc.phan@acronics.com>
2012-10-18 16:29:46 -0700

    No clue what was changed

--- src/com/axcoto/shinjuku/sushi/RootActivity.java
+++ src/com/axcoto/shinjuku/sushi/RootActivity.java
@@ -4,6 +4,10 @@ import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputConnection;
 
 public class RootActivity extends Activity {
 
