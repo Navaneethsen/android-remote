a6fc0633e69be5696554538a68d2361ec3ed8126
Vinh <kureikain@gmail.com>
2013-08-07 11:44:13 -0700

    * Store information in dedicated AppInfo class

--- src/com/ceenee/maki/AppInfo.java
+++ src/com/ceenee/maki/AppInfo.java
@@ -2,8 +2,19 @@ package com.ceenee.maki;
 
 public class AppInfo {
 	//VERSIONING USE http://semver.org/
+	public static final String CODENAME = "HAJIME";
 	public static final int VERSION_MAJOR = 1;
 	public static final int VERSION_MINOR = 0;
-	public static final int VERSION_PATCH = 2;
-	public static final String VERSION = "1.0.0-rc2"; //an alias as a quickway to get version number instead of concat string
+	public static final int VERSION_PATCH = 14;
+	public static final String VERSION = "1.0.14-rc2"; //an alias as a quickway to get version number instead of concat string
+	
+	public final static int PHASE_EMULATOR = 1;
+	public final static int PHASE_DEVELOPMENT = 2;
+	public final static int PHASE_PRODUCTION = 3;
+
+//	public final static int ENVIRONMENT = PHASE_EMULATOR;
+	public final static int ENVIRONMENT = PHASE_DEVELOPMENT;
+//	 public final static int ENVIRONMENT = PHASE_PRODUCTION;
+
+	
 }
